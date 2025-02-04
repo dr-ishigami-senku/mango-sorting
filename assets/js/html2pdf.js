aaf3275fc39370cb3fe1937d0f8c6391164ffaa0
dr-ishigami-senku <nu522696@gmail.com>
2025-02-04 15:45:51 +0800

    pdf file save

--- assets/js/html2pdf.js
+++ assets/js/html2pdf.js
@@ -1,6 +1,16 @@
 const elementDl = document.getElementById('element-to-print');
 const downloadBtn = document.getElementById('download-btn');
+const dashboardText = document.createElement('p');
+dashboardText.classList.add("text-center", "fs-4");
+const combinedContainer = document.createElement('div');
 
 downloadBtn.addEventListener('click', function() {
-    html2pdf(elementDl);
+    dashboardText.textContent = localStorage.getItem("name") + "'s Table";
+    combinedContainer.appendChild(dashboardText.cloneNode(true));
+    combinedContainer.appendChild(elementDl.cloneNode(true));
+
+    const options = {
+        filename: localStorage.getItem("name").toLowerCase() + '-table.pdf'
+    };
+    html2pdf(combinedContainer, options);
 });
