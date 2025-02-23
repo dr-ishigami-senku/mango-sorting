386551264d41766186930e67fdb7ab6108d52f72
dr-ishigami-senku <nu522696@gmail.com>
2025-02-23 12:19:19 +0800

    .

--- assets/js/insert.js
+++ assets/js/insert.js
@@ -7,16 +7,51 @@ insertBtn.addEventListener('click', function () {
         const merchant_list = database.ref('merchants/');
         merchant_list.once('value', (snapshot) => {
             const values = snapshot.val();
-            const textExists = Object.keys(values).includes(input.value.charAt(0).toUpperCase() + input.value.slice(1));
 
-            if (textExists) {
-                const noteModal = new bootstrap.Modal(document.getElementById('note-modal'));
-                noteModal.show();
+            if (values !== null) {
+                const textExists = Object.keys(values).includes(input.value.charAt(0).toUpperCase() + input.value.slice(1));
+
+                if (textExists) {
+                    const noteModal = new bootstrap.Modal(document.getElementById('note-modal'));
+                    noteModal.show();
+                } else {
+                    const insertRef = database.ref('merchants/' + input.value.charAt(0).toUpperCase() + input.value.slice(1) + '/');
+
+                    insertRef.set({
+                        archive: false,
+                        table: {
+                            totalRow: 0
+                        }
+                    });
+
+                    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+                    const today = new Date().toLocaleDateString('en-US', options);
+                    const historyRef = database.ref('merchants/' + input.value.charAt(0).toUpperCase() + input.value.slice(1) + '/history/' + today +'/');
+
+                    historyRef.set({
+                        unripe: {
+                            small: 1,
+                            medium: 2,
+                            large: 3,
+                            total: 4
+                        },
+                        ripe: {
+                            small: 5,
+                            medium: 6,
+                            large: 7,
+                            total: 8
+                        },
+                        decay: 9
+                    });
+                }
             } else {
                 const insertRef = database.ref('merchants/' + input.value.charAt(0).toUpperCase() + input.value.slice(1) + '/');
 
                 insertRef.set({
-                    archive: false
+                    archive: false,
+                    table: {
+                        totalRow: 0
+                    }
                 });
 
                 const options = { year: 'numeric', month: 'long', day: 'numeric' };
